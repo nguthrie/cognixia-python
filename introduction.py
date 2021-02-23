2710a90c566ab7136882a2e52d17979d49714195
nguthrie <nicholas.guthrie4@gmail.com>
2021-02-23 16:34:55 -0600

    dataype intro

--- introduction.py
+++ introduction.py
@@ -1,9 +1,17 @@
 name = "John"
 
-print("Hello", name + "!")
+# I am a comment line
+
+# print("Hello", name + "!")
 
 a = 5
 b = 5
-print(a + b)
-print(a - b)
-print(a % b)
+# print(a + b)
+# print(a - b)
+# print(a % b)
+
+a = 5
+b = 5.0
+c = "5"
+d = True
+
