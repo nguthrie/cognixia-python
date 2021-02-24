8e1461886b426f93db58ad77f13390bbad92bdb0
nguthrie <nicholas.guthrie4@gmail.com>
2021-02-24 15:04:01 -0600

    initial commit

---
+++ branching_logic.py
@@ -0,0 +1,13 @@
+like_python = False
+like_netflix = False
+
+if like_python:
+    print("I will study now")
+elif like_netflix:
+    print("I will relax now")
+elif False:
+    print("second elif statement")
+else:
+    print("If everything is false above me, I will print this statement")
+
+
