4e65ae6693c78032edd8709fc16908142c642236
nguthrie <nicholas.guthrie4@gmail.com>
2021-02-25 16:19:16 -0600

    Added partial CLI funcitonality for calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 import math
 
 class Calculator():
-
+    
     last = None
     mem = None
 
@@ -35,30 +35,59 @@ class Calculator():
     def from_memory(self):
         self.last = self.mem
 
-
-if __name__ == "__main__":
-    
-    my_calc = Calculator()
+def tests():
+    '''
+    First pass of tests for most functions.
+    '''
+    my_calc = Calculator()  # created an object from the class Calculator
     print(my_calc.add(1, 2))
-    print(my_calc.last)
-   
+    print(my_calc.last)     # 3
+
     # square of 3
     print(my_calc.exp(3, 2))
-    print(my_calc.last)
+    print(my_calc.last)     # 9
     
     # store in memory
     my_calc.to_memory()
-    print(my_calc.mem)
+    print(my_calc.mem)      # 9 
     
     # new operation
     my_calc.subtract(10, 10)
-    print(my_calc.last)
-   
+    print(my_calc.last)     # 0
+
     # recall from mem
     my_calc.from_memory()
-    print(my_calc.last)
-   
+    print(my_calc.last)     # 9
+
     # square the number in memory
     my_calc.sqrt(my_calc.last)
-    print(my_calc.last)
+    print(my_calc.last)     # 3
+
+if __name__ == "__main__":
+
+    # tests()
+    
+    my_calc2 = Calculator()
+
+    continue_calc = True
+
+    while continue_calc:
+
+        if continue_calc == "N":
+            break
+
+        num1, comma, num2 = tuple(input("Please enter two comma seperated numbers: "))
+        num1, num2 = float(num1), float(num2)
+        print("1:Add\n2:Subtract\n3:Division\n4:Multiplcation\5:Exponent\6:Square Root")
+        opp = int(input("Please enter a command number from above."))
+
+        if opp == 1:
+            print(my_calc2.add(num1, num2))
+        
+        continue_calc = input("Continue? y/N ")
+
+        
+        
+
+
 
