dfb4298a651625ff967eb2ad1e96b80ae13b6b47
nguthrie <nicholas.guthrie4@gmail.com>
2021-02-25 11:24:29 -0600

    initial commit

---
+++ loops.py
@@ -0,0 +1,28 @@
+list_of_numbers = [0, 1, 2, 'blue', 4]
+
+
+# for loops
+for elem in list_of_numbers:
+    if type(elem) == str:
+        continue
+    print(elem)
+
+
+# range loop
+# for i in range(5):
+#     print(i)
+
+
+# while loop
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# i = 0
+# while True:
+#     i += 1
+#     print(i)
+#     if i > 10:
+#         break
